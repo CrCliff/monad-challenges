65e29996eec8ba61e17d9611acb08ddd22dfcfa0
CrCliff <crcliff@comcast.net>
2020-03-23 18:17:30 -0400

    Set 1 partially completed

--- src/Set1.hs
+++ src/Set1.hs
@@ -4,7 +4,63 @@
 module Set1 where
 
 import MCPrelude
+import Data.Monoid (mconcat)
+
+type Gen a = Seed -> (a, Seed)
 
 fiveRands :: [Integer]
-fiveRands = [0]
+fiveRands = map fst [x1,x2,x3,x4,x5]
+  where 
+    x1 = rand $ mkSeed 1
+    x2 = rand $ snd x1
+    x3 = rand $ snd x2
+    x4 = rand $ snd x3
+    x5 = rand $ snd x4
+
+randLetter   :: Gen Char 
+randLetter s = (toLetter $ fst x, snd x)
+  where
+    x = rand s
+
+randString3 :: String
+randString3 = foldr (:) [] $ map fst [x1, x2, x3]
+  where
+    x1 = randLetter $ mkSeed 1
+    x2 = randLetter $ snd x1
+    x3 = randLetter $ snd x2
+
+
+generalA     :: (Integer -> Integer) -> Gen Integer
+generalA f s = (f $ fst x, snd x)
+  where x = rand s
+
+randEven :: Gen Integer
+randEven = generalA (*2)
+
+randOdd :: Gen Integer
+randOdd = generalA ((+1) . (*2))
+
+randTen :: Gen Integer
+randTen = generalA (*10)
+
+randPair :: Gen (Char, Integer)
+randPair s = ((fst x, fst y), snd y)
+  where
+    x = randLetter s
+    y = rand $ snd x
+
+generalPair :: Gen a -> Gen b -> Gen (a,b)
+generalPair g1 g2 s = ((fst x, fst y), snd y)
+  where
+    x = g1 s
+    y = g2 $ snd x
+
+generalB :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
+generalB f g1 g2 s = (f (fst x) (fst y), snd y)
+  where
+    x = g1 s
+    y = g2 $ snd x
+
+generalPair2 :: Gen a -> Gen b -> Gen (a,b)
+generalPair2 = generalB (,)
 
--- test/Set1Spec.hs
+++ test/Set1Spec.hs
@@ -1,10 +1,43 @@
 -- file Set1Spec.hs
+
+import Data.Digest.Pure.SHA
+import Data.ByteString.Lazy.UTF8 (fromString)
 import Test.Hspec
+import Test.QuickCheck
+import MCPrelude
 import Set1
 
 main :: IO()
 main = hspec $ do
   describe "fiveRands" $ do
     it "product of first five randoms check" $ do
-      foldl (*) 0 fiveRands `shouldBe` (8681089573064486461641871805074254223660 :: Integer)
+      foldl (*) 1 fiveRands `shouldBe` (8681089573064486461641871805074254223660 :: Integer)
+  describe "randString3" $ do
+    it "SHA-256 hash of output of randString3 check" $ do
+      (showDigest . sha256 . fromString) randString3 `shouldBe` "9d475eb78d3e38085220ed6ebde9d8f7d26540bb1c8f9382479c3acd4c8c94a3"
+
+  describe "randEven" $ do
+    it "returns the output of rand * 2" $ do
+      property $ \x -> fst (randEven $ mkSeed x) == fst (rand $ mkSeed (x :: Integer)) * 2
+  describe "randOdd" $ do
+    it "returns the output of rand * 2 + 1" $ do
+      property $ \x -> fst (randOdd $ mkSeed x) == fst (rand $ mkSeed (x :: Integer)) * 2 + 1
+  describe "randTen" $ do
+    it "returns the output of rand * 10" $ do
+      property $ \x -> fst (randTen $ mkSeed x) == fst (rand $ mkSeed (x :: Integer)) * 10
+  describe "randEven, randOdd, randTen" $ do
+    it "product of functions check" $ do
+      foldl (*) 1 (map fst [randEven $ mkSeed 1, randOdd $ mkSeed 1, randTen $ mkSeed 1]) `shouldBe` 189908109902700
+
+  describe "randPair" $ do
+    it "check (mkSeed 1)" $ do
+      fst (randPair (mkSeed 1)) `shouldBe` ('l',282475249)
+  describe "generalPair" $ do
+    it "generalPair is equivalent to randpair when using randLetter and rand" $ do
+      property $ \x -> fst (randPair (mkSeed (x :: Integer))) == fst (generalPair randLetter rand $ mkSeed x)
+  describe "generalPair2" $ do
+    it "generalPair2 is equivalent to randpair when using randLetter and rand" $ do
+      property $ \x -> fst (randPair (mkSeed (x :: Integer))) == fst (generalPair2 randLetter rand $ mkSeed x)
+    it "generalPair2 is equivalent to generalPair" $ do
+      property $ \x -> fst (generalPair randLetter rand $ mkSeed x) == fst (generalPair2 randLetter rand $ mkSeed (x :: Integer))
 
